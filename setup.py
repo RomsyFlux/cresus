15ac757b7b2fcf18f0562aa5290f18d7e5a3e74e
romsyflux <romsyflux@romsyflux.com>
2025-10-26 20:37:35 +0100

    Updating depencies versions

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     python_requires=">=3.11",
     install_requires=requirements,
